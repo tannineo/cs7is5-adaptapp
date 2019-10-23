06af2bef8ce1ae638220677d7fd3ca0b38fee7f6
shrau94 <shrau94@gmail.com>
2019-10-23 12:24:16 +0100

    changes for registration and material design integration

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { MatButtonModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const routes: Routes = [
+  { path: 'register', component: RegistrationComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    FormsModule, 
+    MatButtonModule
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
