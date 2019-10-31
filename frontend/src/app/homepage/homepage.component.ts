1ff3bf431578daade666af98bb46ed63d92738fd
Githu <githujeevasavy@gmail.com>
2019-10-31 00:26:42 +0000

    registration page

--- frontend/src/app/homepage/homepage.component.ts
+++ frontend/src/app/homepage/homepage.component.ts
@@ -9,7 +9,7 @@ export class HomepageComponent implements OnInit {
   imagesList = [];
   constructor() {
     for (let i = 0; i < 50; i++) {
-      const url = 'https://api.unsplash.com/search/photos?page=1&query=office' + (i +1);
+      const url = 'https://loremflickr.com/1200/800?cats' + (i +1);
       this.imagesList[i] = {
         url: url,
         show: false
