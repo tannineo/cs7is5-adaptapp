b5f1020c8c65a2a12d3bed5f4648c8c86d7a4cd0
tannineo <tannineo@outlook.com>
2019-11-01 18:12:43 +0000

    user api WIP

--- server/app/controller/user.py
+++ server/app/controller/user.py
@@ -1,6 +1,8 @@
 from flask import request
 from flask_restplus import Namespace, Resource, fields
 
+from app.service import user
+
 api = Namespace(
     'user',
     description='User Controller, register, login, logout & account settings')
@@ -19,9 +21,18 @@ class UserRegister(Resource):
     @api.doc('user_register', body=register_fields)
     def post(self):
         json = request.get_json()
-        print(json)
 
-        return {'msg': 'OK'}
+        try:
+            pass
+            # TODO
+            # validate the data
+            # register service
+        except RuntimeError as error:
+            pass
+        else:
+            pass
+
+        return {'msg': 'OK', 'result': '23333333'}
 
 
 login_fields = api.model(
--- server/app/service/user.py
+++ server/app/service/user.py
@@ -1,12 +1,21 @@
+import md5
 from app.model.user import User
+
+from app.config import server_config
+
+hash_secret = server_config.get
 
 
 def create_user(username, password, email):
-    """create a user, but check if user exists first"""
+    # create a user, but check if user exists first
     if User.objects(username=username).count() > 0:
         return 'username already exists'
 
-    user = User(username=username, password=password, email=email)
+    # md5 hash the string
+    hashed_password = md5(password + hash_secret).hexdigest()
+
+    # save the user
+    user = User(username=username, password=hashed_password, email=email)
     user.save()
 
     return None
