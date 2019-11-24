cf2fa79217827aa9f47a1da67d9cd2d0407b051a
Githu <githujeevasavy@gmail.com>
2019-11-24 16:14:14 +0000

    Search and Recomendation API

--- frontend/src/app/homepage/homepage.component.ts
+++ frontend/src/app/homepage/homepage.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-import {UserService} from '../shared/user.service';
-import {Router} from '@angular/router';
-
+import { UserService } from '../shared/user.service';
+import { Router } from '@angular/router';
+import { HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-homepage',
@@ -12,6 +13,16 @@ import {Router} from '@angular/router';
   styleUrls: ['../../../node_modules/ng-masonry-grid/ng-masonry-grid.css']
 })
 export class HomepageComponent implements OnInit {
+  apiURL = 'http://34.65.122.100/';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+  @Input() details = {
+    searchValue: '',
+
+  }
   faHeart = faHeart;
   faSearch = faSearch;
   faLogOut = faSignOutAlt;
@@ -20,28 +31,31 @@ export class HomepageComponent implements OnInit {
     'force_pic_config': '',
     'network_status': ''
   };
+  imageSize = 400;
+
+
   imagesList = [{
     url: 'https://source.unsplash.com/user/erondu/1600x900'
   },
-    {
-      url: 'https://source.unsplash.com/user/erondu/1600x900'
-    },
-    {
-      url: 'https://source.unsplash.com/user/erondu/600x900'
-    },
-    {
-      url: 'https://source.unsplash.com/user/erondu/600x900'
-    },
-    {
-      url: 'https://source.unsplash.com/user/erondu/600x900'
-    }];
+  {
+    url: 'https://source.unsplash.com/user/erondu/1600x900'
+  },
+  {
+    url: 'https://source.unsplash.com/user/erondu/600x900'
+  },
+  {
+    url: 'https://source.unsplash.com/user/erondu/600x900'
+  },
+  {
+    url: 'https://source.unsplash.com/user/erondu/600x900'
+  }];
   recommendedList = [{
     url: 'https://source.unsplash.com/user/erondu/1000x700'
   },
-    {
-      url: 'https://source.unsplash.com/user/erondu/1000x700'
-    }];
-  constructor(private router: Router, private userService: UserService) {
+  {
+    url: 'https://source.unsplash.com/user/erondu/1000x700'
+  }];
+  constructor(private router: Router, private userService: UserService, private http: HttpClient) {
   }
   ngOnInit() {
     this.logNetworkInfo();
@@ -56,7 +70,47 @@ export class HomepageComponent implements OnInit {
     console.log(img);
     console.log(i);
   }
+  recommendedImages() {
+    this.http.get(this.apiURL + 'picture/recommend', this.httpOptions)
+      .subscribe((data: any) => {
+        if (data.msg === 'OK') {
+
+          var i = 0;
+          while (i < data.result.pictures.length) {
+            this.recommendedList[i].url = data.result.prefix + this.imageSize + data.result.pictures[i].url;
+            i++;
+          }
+        }
+        else {
+          alert("No Recommendations Found");
+        }
+      })
+  }
   searchClicked(e) {
+    this.httpOptions.headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': localStorage.getItem('currentUser'),
+    });
+    if (this.details.searchValue != '') {
+      this.http.get(this.apiURL + 'picture/search?search=' + this.details.searchValue, this.httpOptions)
+        .subscribe((data: any) => {
+          if (data.msg === 'OK') {
+
+            var i = 0;
+            while (i < data.result.pictures.length) {
+              this.imagesList[i].url = data.result.prefix + this.imageSize + data.result.pictures[i].url;
+              i++;
+            }
+          }
+          else {
+            alert("No Results Found");
+          }
+        })
+      this.recommendedImages();
+    }
+    else {
+      alert("Enter search value");
+    }
   }
   logOutClicked(e) {
     this.userService.logoutUser()
